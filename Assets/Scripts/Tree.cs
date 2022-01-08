af73a51e552a3ea626ee53673498fa868cb8f40c
Jun-Hyeok <jun.hyeok@kakao.com>
2022-01-09 07:43:08 +0900

    Update asset
    version: 0.0.5
    add feat: Create trees at random locations

--- Assets/Scripts/Tree.cs
+++ Assets/Scripts/Tree.cs
@@ -2,26 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Food : MonoBehaviour
+public class Tree : MonoBehaviour
 {
     float objects_initial_range = 60.0f;
-    float objects_initial_height = 1.0f;
-    Rigidbody rb;
+    float objects_initial_height = 0.0f;
 
     // Start is called before the first frame update
     void Start()
     {
-        rb = this.GetComponent<Rigidbody>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (transform.position.y < -1.0f)
-        {
-            transform.position = this.GetPos();
-            rb.velocity = Vector3.zero;
-        }
+
     }
 
     public Vector3 GetPos()
