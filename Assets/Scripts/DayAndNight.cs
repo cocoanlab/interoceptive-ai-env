bfb2f715d5002789e9199feb1a32accd2399c99c
Jun-Hyeok <jun.hyeok@kakao.com>
2022-01-09 08:37:30 +0900

    Update asset
    version: 0.0.6
    add feat: day and night

---
+++ Assets/Scripts/DayAndNight.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DayAndNight : MonoBehaviour
+{
+    [SerializeField] private float secondPerRealTimeSecound; // 유니티 안에서의 100초 = 현실 세계의 1
+
+    private bool isNight = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.right, 0.1f * secondPerRealTimeSecound * Time.deltaTime);
+    }
+}
--- Assets/Scripts/ForagerAgent.cs
+++ Assets/Scripts/ForagerAgent.cs
@@ -1,47 +1,117 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using Unity.MLAgents;
 using Unity.MLAgents.Sensors;
-
+// using TMPro;
 
 public class ForagerAgent : Agent
 {
-    private int NUM_RESOURCES = 2;
+    // [Header("Energy Text Setting")]
+    // public TMP_Text foodLevel;
+    // public TMP_Text waterLevel;
 
-    private float forward_speed = 5f;
-    private float rotation_angle_speed = 200f;
-    private float resource_decreasing_rate = 0.2f;  // [unit/sec]
+    [Header("Status")]
+    public float maxHunger = 15.0f;
+    public float minHunger = -15.0f;
+    public float hungerFallRate = 0.2f;
+    public float maxThirst = 15.0f;
+    public float minThirst = -15.0f;
+    public float thirstFallRate = 0.2f;
+    private float hungerLevel;
+    private float thirstLevel;
+    //
+    public int numResources = 2;
+    public float resourceDecreaseRate = 0.2f;  // [unit/sec]
+    public float resourceLimit = 15.0f; // Episode Terminate if the agent exceed this limit
+    private float[] resourceLevel;
+
+    [Header("Movement")] 
+    public float moveSpeed = 5.0f;
+    public float rotateSpeed = 200.0f;
+    private float descentVelocity;
     private CharacterController character;
-    private float vertial_velocity;
-    private bool taking_eat_behavior;
-    private float resource_limit = 15f; // Episode Terminate if the agent exceed this limit
 
-    // Resource Parameters
-    public GameObject prefab_red;
-    public GameObject prefab_blue;
-    public GameObject prefab_tree;
-    private float food_red_increase = 3.0f;
-    private float food_blue_increase = 3.0f;
-    private int num_resource_red = 400;
-    private int num_resource_blue = 400;
-    //private int num_resource_red = 200;
-    //private int num_resource_blue = 200;
-    //public int num_resource_red = (int)Academy.Instance.EnvironmentParameters.GetWithDefault("num_resources", 400.0f);
-    //public int num_resource_blue = (int)Academy.Instance.EnvironmentParameters.GetWithDefault("num_resources", 400.0f);
-    private int num_tree = 100;
-    private float[] resource_levels;
-    private GameObject[] objects_red;
-    private GameObject[] objects_blue;
-    private GameObject[] objects_tree;
-    private float objects_initial_height= 3.0f;
-    private float objects_initial_range = 60.0f;
+    [Header("Resource")]
+    public GameObject hungerResourceObj;
+    public GameObject thirstResourceObj;
+    private GameObject[] hungerResourceObjs;
+    private GameObject[] thirstResourceObjs;
+    public int numHungerResourceObj = 200;
+    public int numThirstResourceObj = 200;
+    public float hungerResourceIncrease = 3.0f;
+    public float thirstResourceIncrease = 3.0f;
+    public float objInitialHeight= 3.0f;
+    public float objInitialRange = 60.0f;
+    public bool manualEatBehavior = true;
+    // public int numCubes;
+
+    [Header("Obstacle")]
+    public GameObject treeObj;
+    private GameObject[] treeObjs;
+    private int numTrees = 100;
 
     // Monitor
     [DebugGUIPrint, DebugGUIGraph(max: 15f, min: -15f, group: 1, r: 1, g: 0.4f, b: 0.4f)]
-    float resource_red;
+    float resourceRed;
     [DebugGUIPrint, DebugGUIGraph(max: 15f, min: -15f, group: 1, r: 0.4f, g: 0.4f, b: 1)]
-    float resource_blue;
+    float resourceBlue;
+
+    public override void Initialize()
+    {
+        // base.Initialize();
+
+        //Initialize Agent Position
+        // InitializeAgentPosition();
+
+        //Initialize cubes
+        // for (int i = 0; i < numCubes; i++)
+        // {
+        //     survivalAgent.GetComponent<ItemGeneration>().InitializeItem("Food", cubes);
+        //     survivalAgent.GetComponent<ItemGeneration>().InitializeItem("Water", cubes);
+        // }
+
+        //Initialize Energy Level
+        thirstLevel = 0;
+        hungerLevel = 0;
+
+        //Setting Variables from Python (ml-agents support this)
+        var envParameters = Academy.Instance.EnvironmentParameters;
+        // float MAX_STEP = envParameters.GetWithDefault("MAX_STEP", 10000.0f);
+        float MAX_HUNGER = envParameters.GetWithDefault("MAX_HUNGER", 15.0f);
+        float MIN_HUNGER = envParameters.GetWithDefault("MIN_HUNGER", -15.0f);
+        float HUNGER_FALL_RATE = envParameters.GetWithDefault("HUNGER_FALL_RATE", 0.2f);
+        float MAX_THIRST = envParameters.GetWithDefault("MAX_THIRST", 15.0f);
+        float MIN_THIRST = envParameters.GetWithDefault("MIN_THIRST", -15.0f);
+        float THIRST_FALL_RATE = envParameters.GetWithDefault("THIRST_FALL_RATE", 0.2f);
+        // float NUM_CUBE = envParameters.GetWithDefault("NUM_CUBE", 80.0f);
+        // float WATER_ENERGY = envParameters.GetWithDefault("WATER_ENERGY", 60.0f);
+        // float FOOD_ENERGY = envParameters.GetWithDefault("FOOD_ENERGY", 60.0f);
+        float MOVE_SPEED = envParameters.GetWithDefault("MOVE_SPEED", 5.0f);
+        float ROTATE_SPEED = envParameters.GetWithDefault("ROTATE_SPEED", 200.0f);
+        // float FOCAL_LENGHT = envParameters.GetWithDefault("FOCAL_LENGHT", 10.0f);
+        // float DECISION_PERIOD = envParameters.GetWithDefault("DECISION_PERIOD", 5.0f);
+        float MANUAL_EAT_BEHAVIOR = envParameters.GetWithDefault("MANUAL_EAT_BEHAVIOR", 1.0f);
+
+        // this.MaxStep = (int)MAX_STEP;
+        maxHunger = MAX_HUNGER;
+        minHunger = MIN_HUNGER;
+        hungerFallRate = HUNGER_FALL_RATE;
+        maxThirst = MAX_THIRST;
+        minThirst = MIN_THIRST;
+        thirstFallRate = THIRST_FALL_RATE;
+        // numCubes = (int)NUM_CUBE;
+        // hungerResourceObj.GetComponent<ItemProperties>().value = FOOD_ENERGY;
+        // thirstResourceObj.GetComponent<ItemProperties>().value = WATER_ENERGY;
+        moveSpeed = MOVE_SPEED;
+        rotateSpeed = ROTATE_SPEED;
+        // agentViewCamera.focalLength = FOCAL_LENGHT;
+        // decisionRequet.DecisionPeriod = (int)DECISION_PERIOD;
+        manualEatBehavior = System.Convert.ToBoolean(MANUAL_EAT_BEHAVIOR);
+
+        Debug.Log("Initializing");
+    }
 
     private void Awake()
     {
@@ -59,95 +129,97 @@ public class ForagerAgent : Agent
     void Start()
     {
         print("Start");
-        this.resource_levels = new float[this.NUM_RESOURCES];
+        this.resourceLevel = new float[this.numResources];
         this.character = gameObject.GetComponent<CharacterController>();
-        this.taking_eat_behavior = false;
+        this.manualEatBehavior = false;
 
-        // Generate Food Objects
-        this.objects_red = new GameObject[this.num_resource_red];
-        for (int i = 0; i < this.num_resource_red; i++)
+        // Generate Hunger Objects
+        this.hungerResourceObjs = new GameObject[this.numHungerResourceObj];
+        for (int i = 0; i < this.numHungerResourceObj; i++)
         {
-            GameObject obj = Instantiate(prefab_red) as GameObject;
-            objects_red[i] = obj;
-            objects_red[i].transform.position = GetPos();
-            objects_red[i].transform.Rotate(0, Random.Range(0f, 90.0f), 0);
-            objects_red[i].GetComponent<Rigidbody>().velocity = Vector3.zero;
+            GameObject obj = Instantiate(hungerResourceObj) as GameObject;
+            hungerResourceObjs[i] = obj;
+            hungerResourceObjs[i].transform.position = GetPos();
+            hungerResourceObjs[i].transform.Rotate(0, Random.Range(0f, 90.0f), 0);
+            hungerResourceObjs[i].GetComponent<Rigidbody>().velocity = Vector3.zero;
         }
 
-        this.objects_blue = new GameObject[this.num_resource_blue];
-        for (int i = 0; i < this.num_resource_blue; i++)
+        // Generate Thirst Objects
+        this.thirstResourceObjs = new GameObject[this.numThirstResourceObj];
+        for (int i = 0; i < this.numThirstResourceObj; i++)
         {
-            GameObject obj = Instantiate(prefab_blue) as GameObject;
-            objects_blue[i] = obj;
-            objects_blue[i].transform.position = GetPos();
-            objects_blue[i].transform.Rotate(0, Random.Range(0f, 90.0f), 0);
-            objects_blue[i].GetComponent<Rigidbody>().velocity = Vector3.zero;
+            GameObject obj = Instantiate(thirstResourceObj) as GameObject;
+            thirstResourceObjs[i] = obj;
+            thirstResourceObjs[i].transform.position = GetPos();
+            thirstResourceObjs[i].transform.Rotate(0, Random.Range(0f, 90.0f), 0);
+            thirstResourceObjs[i].GetComponent<Rigidbody>().velocity = Vector3.zero;
         }
 
-        //Generate Tree Objects
-        this.objects_tree = new GameObject[this.num_tree];
-        for (int i = 0; i < this.num_tree; i++)
+        // Generate Tree Objects
+        this.treeObjs = new GameObject[this.numTrees];
+        for (int i = 0; i < this.numTrees; i++)
         {
-            GameObject obj = Instantiate(prefab_tree) as GameObject;
-            objects_tree[i] = obj;
-            objects_tree[i].transform.position = GetPos();
+            GameObject obj = Instantiate(treeObj) as GameObject;
+            treeObjs[i] = obj;
+            treeObjs[i].transform.position = GetPos();
         }
 
     }
 
     public Vector3 GetPos()
     {
-        return new Vector3(Random.Range(-this.objects_initial_range, this.objects_initial_range),
-                        this.objects_initial_height,
-                        Random.Range(-this.objects_initial_range, this.objects_initial_range));
+        return new Vector3(Random.Range(-this.objInitialRange, this.objInitialRange),
+                        this.objInitialHeight,
+                        Random.Range(-this.objInitialRange, this.objInitialRange));
     }
 
     public Vector3 GetPos(float offset_y)
     {
-        return new Vector3(Random.Range(-this.objects_initial_range, this.objects_initial_range),
-                        this.objects_initial_height + offset_y,
-                        Random.Range(-this.objects_initial_range, this.objects_initial_range));
+        return new Vector3(Random.Range(-this.objInitialRange, this.objInitialRange),
+                        this.objInitialHeight + offset_y,
+                        Random.Range(-this.objInitialRange, this.objInitialRange));
     }
 
     public override void OnEpisodeBegin()
     {
         print("New episode begin");
 
-        for (int i = 0; i < this.NUM_RESOURCES; i++)
+        for (int i = 0; i < this.numResources; i++)
         {
-            this.resource_levels[i] = 0;
+            this.resourceLevel[i] = 0;
         }
-        this.taking_eat_behavior = false;
+        this.manualEatBehavior = false;
 
         // Randomize agent position
-        this.transform.position = new Vector3(0, 1.5f, 0);
+        this.transform.position = new Vector3(0, 2.58f, 0);
         this.transform.Rotate(0, Random.Range(0f, 360.0f), 0);
 
         // Randomize object positions
-        for (int i = 0; i < this.num_resource_red; i++)
+        for (int i = 0; i < this.numHungerResourceObj; i++)
         {
             Vector3 pos = GetPos();
-            objects_red[i].transform.position = pos;
-            objects_red[i].transform.Rotate(0, Random.Range(0f, 90.0f), 0);
-            objects_red[i].GetComponent<Rigidbody>().velocity = Vector3.zero;
+            hungerResourceObjs[i].transform.position = pos;
+            hungerResourceObjs[i].transform.Rotate(0, Random.Range(0f, 90.0f), 0);
+            hungerResourceObjs[i].GetComponent<Rigidbody>().velocity = Vector3.zero;
         }
-        for (int i = 0; i < this.num_resource_blue; i++)
+        for (int i = 0; i < this.numThirstResourceObj; i++)
         {
             Vector3 pos = GetPos();
-            objects_blue[i].transform.position = pos;
-            objects_blue[i].transform.Rotate(0, Random.Range(0f, 90.0f), 0);
-            objects_blue[i].GetComponent<Rigidbody>().velocity = Vector3.zero;
+            thirstResourceObjs[i].transform.position = pos;
+            thirstResourceObjs[i].transform.Rotate(0, Random.Range(0f, 90.0f), 0);
+            thirstResourceObjs[i].GetComponent<Rigidbody>().velocity = Vector3.zero;
         }
-        for (int i = 0; i < this.num_tree; i++)
+        for (int i = 0; i < this.numTrees; i++)
         {
-            Vector3 pos = GetPos(-3.0f);
-            objects_tree[i].transform.position = pos;
+            Vector3 pos = GetPos(-1.0f);
+            treeObjs[i].transform.position = pos;
+            treeObjs[i].transform.Rotate(0, Random.Range(0f, 360.0f), 0);
         }
     }
 
     public override void CollectObservations(VectorSensor sensor)
     {
-        sensor.AddObservation(resource_levels);
+        sensor.AddObservation(resourceLevel);
     }
 
 
@@ -164,26 +236,26 @@ public class ForagerAgent : Agent
 
         if (!this.character.isGrounded)
         {
-            this.vertial_velocity -= 9.81f * Time.fixedDeltaTime;
+            this.descentVelocity -= 9.81f * Time.fixedDeltaTime;
             this.character.Move(new Vector3(0, -9.81f * Time.fixedDeltaTime));
         }
         else
         {
-            this.vertial_velocity = 0;
+            this.descentVelocity = 0;
         }
 
-        for (int i = 0; i < this.NUM_RESOURCES; i++)
+        for (int i = 0; i < this.numResources; i++)
         {
-            this.resource_levels[i] -= this.resource_decreasing_rate * Time.fixedDeltaTime;
+            this.resourceLevel[i] -= this.resourceDecreaseRate * Time.fixedDeltaTime;
         }
-        //print(this.resource_decreasing_rate * Time.deltaTime);  
+        //print(this.resourceDecreaseRate * Time.deltaTime);  
 
 
-        //print("Resource (red, blue) = (" + this.resource_levels[0] + "," + this.resource_levels[1] + ")");
-        Monitor.Log("Red", this.resource_levels[0] / 15f, transform);
-        resource_red = this.resource_levels[0];
-        Monitor.Log("Blue", this.resource_levels[1] / 15f, transform);
-        resource_blue = this.resource_levels[1];
+        //print("Resource (red, blue) = (" + this.resourceLevel[0] + "," + this.resourceLevel[1] + ")");
+        Monitor.Log("Red", this.resourceLevel[0] / 15f, transform);
+        resourceRed = this.resourceLevel[0];
+        Monitor.Log("Blue", this.resourceLevel[1] / 15f, transform);
+        resourceBlue = this.resourceLevel[1];
 
         Physics.Simulate(Time.fixedDeltaTime);
         Application.targetFrameRate = (int)(60.0 * Time.timeScale);
@@ -202,31 +274,30 @@ public class ForagerAgent : Agent
          * 4 : Eat
          * ***/
 
-        this.taking_eat_behavior = false;
+        this.manualEatBehavior = false;
         switch (action)
         {
             case 0:
                 break;
             case 1:
                 Vector3 forward = transform.transform.forward;
-                this.character.Move(this.forward_speed * forward * Time.deltaTime);
+                this.character.Move(this.moveSpeed * forward * Time.deltaTime);
                 break;
             case 2:
-                this.transform.Rotate(0, -this.rotation_angle_speed * Time.deltaTime, 0);
+                this.transform.Rotate(0, -this.rotateSpeed * Time.deltaTime, 0);
                 break;
             case 3:
-                this.transform.Rotate(0, this.rotation_angle_speed * Time.deltaTime, 0);
+                this.transform.Rotate(0, this.rotateSpeed * Time.deltaTime, 0);
                 break;
             case 4:
-                print("Eat Behavior");
-                this.taking_eat_behavior = true;
+                this.manualEatBehavior = true;
                 break;
         }
 
         // Set zero reward (reward will be defined in the learning-side)
         SetReward(0.0f);
-        float max_deviation = Mathf.Max(Mathf.Abs(this.resource_levels[0]), Mathf.Abs(this.resource_levels[1]));
-        if (max_deviation > this.resource_limit )
+        float max_deviation = Mathf.Max(Mathf.Abs(this.resourceLevel[0]), Mathf.Abs(this.resourceLevel[1]));
+        if (max_deviation > this.resourceLimit )
         {
             print("Agent died... (-_-;) ");
             EndEpisode();
@@ -265,16 +336,16 @@ public class ForagerAgent : Agent
     {
         if (tag == "food_red")
         {
-            this.resource_levels[0] += this.food_red_increase;
+            this.resourceLevel[0] += this.hungerResourceIncrease;
         }
         if (tag == "food_blue")
         {
-            this.resource_levels[1] += this.food_blue_increase;
+            this.resourceLevel[1] += this.thirstResourceIncrease;
         }
     }
 
     public bool IsAgentTakingEatBehavior()
     {
-        return this.taking_eat_behavior;
+        return this.manualEatBehavior;
     }
 }
