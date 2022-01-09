7c60179763e15ee6b1706bc51c4d1eacba6b6c0d
Jun-Hyeok <jun.hyeok@kakao.com>
2022-01-09 16:00:00 +0900

    Clean up

--- Assets/Scripts/DayAndNight.cs
+++ Assets/Scripts/DayAndNight.cs
@@ -8,12 +8,6 @@ public class DayAndNight : MonoBehaviour
 
     private bool isNight = false;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
     void Update()
     {
--- Assets/Scripts/Tree.cs
+++ Assets/Scripts/Tree.cs
@@ -7,18 +7,6 @@ public class Tree : MonoBehaviour
     float objects_initial_range = 60.0f;
     float objects_initial_height = 0.0f;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
-
     public Vector3 GetPos()
     {
         return new Vector3(Random.Range(-this.objects_initial_range, this.objects_initial_range),
