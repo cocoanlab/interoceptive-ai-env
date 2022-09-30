06464719d78256f255241bb3e60d241fd523bd9e
Jungmin <jungmin.lee@g.skku.edu>
2022-09-30 14:34:28 +0900

    Dynamic_Weather_System

---
+++ Assets/DynamicWeatherSystem/DynamicWeatherSystem.cs
@@ -0,0 +1,150 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum WeatherState { Change, Sun, Thunder, Mist, Rain, Snow }
+
+[RequireComponent(typeof(AudioSource))]   //필요한 component가 GameObject에 자동으로 추가됨
+public class DynamicWeatherSystem : MonoBehaviour
+{
+    public float switchTimer = 0;
+    public float resetTimer = 3600f; 
+    public float minLightIntensity = 0f; 
+    public float maxLightIntensity = 1f; 
+    private int switchWeather;
+    public AudioSource audiosource;
+    public Light sunLight; 
+    public WeatherState weatherState;
+    public WeatherData [] weatherData;
+    
+    public void Awake()
+    {
+        audiosource = GetComponent<AudioSource>();
+        RenderSettings.fog = true;
+        RenderSettings.fogMode = FogMode.ExponentialSquared;
+        RenderSettings.fogDensity = 0.0f;
+    }
+
+    public void Start()
+    {
+        LoadWeatherSystem();
+        StartCoroutine((StartDynamicWeather()));
+
+    }
+
+    void LoadWeatherSystem()
+    {
+        for (int i = 0; i < weatherData.Length; i++)
+        {
+            weatherData[i].emission = weatherData[i].particleSystem.emission;
+        }
+        switchTimer = resetTimer;
+
+    }
+    
+    private void FixedUpdate()
+    {
+        switchTimer -= Time.deltaTime;
+        if (switchTimer > 0) return;
+        if (switchTimer <= 0)
+        {
+            switchTimer = 0; 
+            weatherState = WeatherState.Change;
+            switchTimer = resetTimer;
+        }
+    }
+
+    IEnumerator StartDynamicWeather()
+    {   
+        while (true)
+        {
+            if (weatherState == WeatherState.Change) {SelectWeather(); }
+            else if (weatherState == WeatherState.Sun) {ActivedWeather("Sun"); }
+            else if (weatherState == WeatherState.Thunder) {ActivedWeather("Thunder"); }
+            else if (weatherState == WeatherState.Mist) {ActivedWeather("Mist"); }
+            else if (weatherState == WeatherState.Rain) {ActivedWeather("Rain"); }
+            else if (weatherState == WeatherState.Snow) {ActivedWeather("Snow"); }
+            yield return null; 
+        }
+    }
+
+    void ActivedWeather(string weather)
+    {
+        if (weatherData.Length > 0)
+        {
+            for (int i = 0; i < weatherData.Length; i++)
+            {
+                if(weatherData[i].particleSystem != null)
+                {
+                    if (weatherData[i].name == weather)
+                    {
+                        weatherData[i].emission.enabled = true;
+                        weatherData[i].fogColor = RenderSettings.fogColor;
+                        RenderSettings.fogColor = Color.Lerp(weatherData[i].currentFogColor, weatherData[i].fogColor, weatherData[i].fogChangeSpeed * Time.deltaTime);
+
+                        ChangeWeatherSettings(weatherData[i].lightIntensity, weatherData[i].weatherAudio);
+                    }
+
+                }
+            }
+        }
+    }
+
+
+    void SelectWeather()
+    {
+        switchWeather = Random.Range(0, System.Enum.GetValues(typeof(WeatherState)).Length);
+        ResetWeather();
+        if (switchWeather == 0) { weatherState = WeatherState.Change; } 
+        else if (switchWeather == 1) { weatherState = WeatherState.Sun; }
+        else if (switchWeather == 2) { weatherState = WeatherState.Thunder; }
+        else if (switchWeather == 3) { weatherState = WeatherState.Mist; }
+        else if (switchWeather == 4) { weatherState = WeatherState.Rain; }
+        else if (switchWeather == 5) { weatherState = WeatherState.Snow; }
+    }
+
+    void ChangeWeatherSettings(float lightIntensity, AudioClip audioClip)
+    {
+        Light tmpLight = GetComponent<Light>();
+        if(tmpLight.intensity > maxLightIntensity) { tmpLight.intensity -= Time.deltaTime * lightIntensity; }
+        if(tmpLight.intensity < maxLightIntensity) { tmpLight.intensity += Time.deltaTime * lightIntensity; } 
+
+        if(weatherData[switchWeather].useAudio== true)
+        {
+            AudioSource tmpAudio = GetComponent<AudioSource>();
+
+            if (tmpAudio.volume > 0 && tmpAudio.clip != audioClip)
+            {
+                tmpAudio.volume -= Time.deltaTime * weatherData[switchWeather].audioFadeInTimer; 
+            }
+
+            if (tmpAudio.volume == 0)
+            {
+                tmpAudio.Stop();
+                tmpAudio.clip= audioClip;
+                tmpAudio.loop = true;
+                tmpAudio.Play();
+            }
+
+            if (tmpAudio.volume < 1 && tmpAudio.clip != audioClip)
+            {
+                tmpAudio.volume -= Time.deltaTime * weatherData[switchWeather].audioFadeInTimer;
+            }
+        }
+    }
+    
+    void ResetWeather()
+    {
+        if (weatherData.Length > 0)
+        {
+            for (int i = 0; i < weatherData.Length; i++)
+            {
+                 if (weatherData[i].emission.enabled != false)
+                 {
+                    weatherData[i].emission.enabled = false;
+                 }
+            }
+        }
+    }
+}
+
---
+++ Assets/DynamicWeatherSystem/WeatherData.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public struct WeatherData
+{
+    public string name;
+    public ParticleSystem particleSystem;
+    [HideInInspector]
+    public ParticleSystem.EmissionModule emission;
+    
+    public bool useAudio;
+    public AudioClip weatherAudio;
+    public float audioFadeInTimer;
+    public Transform windzone;
+    public float lightIntensity;
+    public float lightDimTimer;
+    public float fogChangeSpeed;
+    public Color fogColor;
+    public Color currentFogColor;
+
+}
