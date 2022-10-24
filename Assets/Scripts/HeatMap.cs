29b4b912ce3d128e9987c38bfbf5c263d56ea192
Sungwoo-Lee <sungwoo320@gmail.com>
2022-10-25 00:52:02 +0900

    Add turn on/off Heat GUI

--- Assets/Scripts/HeatMap.cs
+++ Assets/Scripts/HeatMap.cs
@@ -5,82 +5,94 @@ using UnityEngine.UI;
 
 public class HeatMap : MonoBehaviour
 {
-    public Image heatMap;
-    public GameObject area;
-    public Gradient gradient;
-    private Texture2D mapTexture;
+        public Image heatMap;
+        public GameObject area;
+        public Gradient gradient;
+        private Texture2D mapTexture;
 
-    // private int count = 0;
-    // private int a = 10;
-    // private int b = 10;
+        public GameObject agentTrack;
 
-    void Awake()
-    {
-        mapTexture = new Texture2D(100, 100);
-    }
+        public FoodCollectorAgent agent;
 
-    public void EpisodeHeatMap()
-    {
+        // private int count = 0;
+        // private int a = 10;
+        // private int b = 10;
 
-        ModifyPixels();
-
-        // count += 1;
-        // Debug.Log("Episode : " + count.ToString());
-        // Debug.Log("Test : " + area.GetComponent<AreaTempSmoothing>().GetNormalizedAreaTemp(10, 10).ToString());
-
-        // b = b + 10;
-        // a = a + 10;
-
-        // Debug.Log("a : " + a.ToString() + "b : " + b.ToString());
-
-    }
-
-    public void ModifyPixels()
-    {
-        for (int z = 0; z < mapTexture.height; z++)
+        void Awake()
         {
-            for (int x = 0; x < mapTexture.width; x++)
-            {
-                Color color = gradient.Evaluate(1 - area.GetComponent<AreaTempSmoothing>().GetNormalizedAreaTemp(x, z));
-                mapTexture.SetPixel(z, -x, color);
-            }
+                if (agent.useThermalObs)
+                {
+                        mapTexture = new Texture2D(100, 100);
+                }
+                else
+                {
+                        heatMap.enabled = false;
+                        agentTrack.GetComponent<Image>().enabled = false;
+                }
         }
 
-        // mapTexture.SetPixel(b, -a, Color.black);
+        public void EpisodeHeatMap()
+        {
 
-        mapTexture.Apply();
+                ModifyPixels();
 
-        heatMap.material.mainTexture = mapTexture;
-    }
+                // count += 1;
+                // Debug.Log("Episode : " + count.ToString());
+                // Debug.Log("Test : " + area.GetComponent<AreaTempSmoothing>().GetNormalizedAreaTemp(10, 10).ToString());
 
-    // void Start()
-    // {
-    //     Renderer rend = GetComponent<Renderer>();
+                // b = b + 10;
+                // a = a + 10;
 
-    //     // duplicate the original texture and assign to the material
-    //     Texture2D texture = Instantiate(rend.material.mainTexture) as Texture2D;
-    //     rend.material.mainTexture = texture;
+                // Debug.Log("a : " + a.ToString() + "b : " + b.ToString());
 
-    //     // colors used to tint the first 3 mip levels
-    //     Color[] colors = new Color[3];
-    //     colors[0] = Color.red;
-    //     colors[1] = Color.green;
-    //     colors[2] = Color.blue;
-    //     int mipCount = Mathf.Min(3, texture.mipmapCount);
+        }
 
-    //     // tint each mip level
-    //     for (int mip = 0; mip < mipCount; ++mip)
-    //     {
-    //         Color[] cols = texture.GetPixels(mip);
-    //         for (int i = 0; i < cols.Length; ++i)
-    //         {
-    //             cols[i] = Color.Lerp(cols[i], colors[mip], 0.33f);
-    //         }
-    //         texture.SetPixels(cols, mip);
-    //     }
-    //     // actually apply all SetPixels, don't recalculate mip levels
-    //     texture.Apply(false);
-    // }
+        public void ModifyPixels()
+        {
+                for (int z = 0; z < mapTexture.height; z++)
+                {
+                        for (int x = 0; x < mapTexture.width; x++)
+                        {
+                                Color color = gradient.Evaluate(1 - area.GetComponent<AreaTempSmoothing>().GetNormalizedAreaTemp(x, z));
+                                mapTexture.SetPixel(z, -x, color);
+                        }
+                }
+
+                // mapTexture.SetPixel(b, -a, Color.black);
+
+                mapTexture.Apply();
+
+                heatMap.material.mainTexture = mapTexture;
+        }
+
+        // void Start()
+        // {
+        //     Renderer rend = GetComponent<Renderer>();
+
+        //     // duplicate the original texture and assign to the material
+        //     Texture2D texture = Instantiate(rend.material.mainTexture) as Texture2D;
+        //     rend.material.mainTexture = texture;
+
+        //     // colors used to tint the first 3 mip levels
+        //     Color[] colors = new Color[3];
+        //     colors[0] = Color.red;
+        //     colors[1] = Color.green;
+        //     colors[2] = Color.blue;
+        //     int mipCount = Mathf.Min(3, texture.mipmapCount);
+
+        //     // tint each mip level
+        //     for (int mip = 0; mip < mipCount; ++mip)
+        //     {
+        //         Color[] cols = texture.GetPixels(mip);
+        //         for (int i = 0; i < cols.Length; ++i)
+        //         {
+        //             cols[i] = Color.Lerp(cols[i], colors[mip], 0.33f);
+        //         }
+        //         texture.SetPixels(cols, mip);
+        //     }
+        //     // actually apply all SetPixels, don't recalculate mip levels
+        //     texture.Apply(false);
+        // }
 }
 
 
--- Assets/Scripts/ResourceUI.cs
+++ Assets/Scripts/ResourceUI.cs
@@ -2,60 +2,65 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 // GameObject인 Canvas의 하위 GameObject인 ResourceLevelSetting에 부착됨
 public class ResourceUI : MonoBehaviour
 {
-    public Slider BlueLevel;
-    public Slider RedLevel;
-    public Slider YellowLevel;
+        public Slider BlueLevel;
+        public Slider RedLevel;
+        public Slider YellowLevel;
 
-    public Image Bluehandle;
-    public Image Redhandle;
-    public Image Yellowhandle;
+        public Image Bluehandle;
+        public Image Redhandle;
+        public Image Yellowhandle;
 
-    public FoodCollectorAgent agent;
+        public GameObject BlueText;
+        public GameObject RedText;
+        public GameObject YellowText;
 
-    private float blueLevel;
-    private float redLevel;
-    private float yellowLevel;
+        public FoodCollectorAgent agent;
 
-    void Start()
-    {
-        BlueLevel.value = 0;
-        RedLevel.value = 0;
-        Bluehandle.color = Color.blue;
-        Redhandle.color = Color.red;
+        private float blueLevel;
+        private float redLevel;
+        private float yellowLevel;
 
-        if (agent.useThermalObs)
+        void Start()
         {
-            YellowLevel.value = 0;
-            Yellowhandle.color = Color.yellow;
-        }
-        else
-        {
-            YellowLevel.enabled = false;
-            Yellowhandle.enabled = false;
-        }
+                BlueLevel.value = 0;
+                RedLevel.value = 0;
+                Bluehandle.color = Color.blue;
+                Redhandle.color = Color.red;
 
-    }
+                if (agent.useThermalObs)
+                {
+                        YellowLevel.value = 0;
+                        Yellowhandle.color = Color.yellow;
+                }
+                else
+                {
+                        YellowLevel.gameObject.SetActive(false);
+                        YellowText.GetComponent<TextMeshProUGUI>().enabled = false;
+                }
 
-    void Update()
-    {
-        redLevel = agent.ResourceLevels[0];
-        blueLevel = agent.ResourceLevels[1];
-
-        BlueLevel.value = blueLevel;
-        RedLevel.value = redLevel;
-
-        if (agent.useThermalObs)
-        {
-            yellowLevel = agent.ResourceLevels[2];
-            YellowLevel.value = yellowLevel;
         }
 
-        // if (blueLevel >= 0) { Bluehandle.color = Color.green; } else { Bluehandle.color = Color.red; }
-        // if (redLevel >= 0) { Redhandle.color = Color.green; } else { Redhandle.color = Color.red; }
-        // if (yellowLevel >= 0) { Yellowhandle.color = Color.green; } else { Yellowhandle.color = Color.red; }
-    }
+        void Update()
+        {
+                redLevel = agent.ResourceLevels[0];
+                blueLevel = agent.ResourceLevels[1];
+
+                BlueLevel.value = blueLevel;
+                RedLevel.value = redLevel;
+
+                if (agent.useThermalObs)
+                {
+                        yellowLevel = agent.ResourceLevels[2];
+                        YellowLevel.value = yellowLevel;
+                }
+
+                // if (blueLevel >= 0) { Bluehandle.color = Color.green; } else { Bluehandle.color = Color.red; }
+                // if (redLevel >= 0) { Redhandle.color = Color.green; } else { Redhandle.color = Color.red; }
+                // if (yellowLevel >= 0) { Yellowhandle.color = Color.green; } else { Yellowhandle.color = Color.red; }
+        }
 }
