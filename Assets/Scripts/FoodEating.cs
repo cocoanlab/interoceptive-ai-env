628ecbf88e431070de6780413b62528515b88829
Sungwoo-Lee <sungwoo320@gmail.com>
2022-11-29 20:30:02 +0900

    Fixed cube blue issues

--- Assets/Scripts/FoodEating.cs
+++ Assets/Scripts/FoodEating.cs
@@ -5,37 +5,45 @@ using UnityEngine;
 // GameObject인 FoodEatRange에 부착함
 public class FoodEating : MonoBehaviour
 {
-    public FoodCollectorAgent agent;
-    public FoodCollectorArea myArea;
+        public FoodCollectorAgent agent;
+        public FoodCollectorArea myArea;
 
-    // Agent의 앞쪽에 sphere collider가 있는데 그것의 isTrigger가 켜져있고 다른 collider가 들어왔는지 감지함
-    public void OnTriggerStay(Collider other)
-    {
-        bool isEaten = false;
-        if (agent.IsAutoEat || agent.IsEat)
+        // Agent의 앞쪽에 sphere collider가 있는데 그것의 isTrigger가 켜져있고 다른 collider가 들어왔는지 감지함
+        public void OnTriggerStay(Collider other)
         {
-            if (other.CompareTag("food_red"))
-            {
-                agent.IncreaseLevel("food_red");
-                isEaten = true;
-            }
-            else if (other.CompareTag("food_blue"))
-            {
-                agent.IncreaseLevel("food_blue");
-                isEaten = true;
-            }
-        }
+                bool isEaten = false;
+                if (agent.IsAutoEat || agent.IsEat)
+                {
+                        if (other.CompareTag("food_red"))
+                        {
+                                agent.IncreaseLevel("food_red");
+                                isEaten = true;
+                        }
+                        else if (other.CompareTag("food_blue"))
+                        {
+                                agent.IncreaseLevel("food_blue");
+                                isEaten = true;
+                        }
+                }
 
-        // 음식을 먹으면 그 음식의 위치가 옮겨짐 (사실상 먹으면 다른 곳에 새로 생기는 것과 비슷한 효과)
-        if (isEaten)
-        {
-            if (other.CompareTag("food_red"))
-            {
-                other.transform.position = new Vector3(Random.Range(-myArea.range, myArea.range),
-                3f, Random.Range(-myArea.range, myArea.range)) + myArea.transform.position;
-                FoodProperty f = other.gameObject.GetComponent<FoodProperty>();
-                f.InitializeProperties();
-            }
+                // 음식을 먹으면 그 음식의 위치가 옮겨짐 (사실상 먹으면 다른 곳에 새로 생기는 것과 비슷한 효과)
+                if (isEaten)
+                {
+                        if (other.CompareTag("food_red"))
+                        {
+                                other.transform.position = new Vector3(Random.Range(-myArea.range, myArea.range),
+                                3f, Random.Range(-myArea.range, myArea.range)) + myArea.transform.position;
+                                FoodProperty f = other.gameObject.GetComponent<FoodProperty>();
+                                f.InitializeProperties();
+                        }
+
+                        else if (other.CompareTag("food_blue"))
+                        {
+                                other.transform.position = new Vector3(Random.Range(-myArea.range, myArea.range),
+                                3f, Random.Range(-myArea.range, myArea.range)) + myArea.transform.position;
+                                FoodProperty f = other.gameObject.GetComponent<FoodProperty>();
+                                f.InitializeProperties();
+                        }
+                }
         }
-    }
 }
--- Assets/Scripts/FoodProperty.cs
+++ Assets/Scripts/FoodProperty.cs
@@ -5,52 +5,52 @@ using MathNet.Numerics;
 // Food 클래스의 멤버 변수 prefab을 선언할 때 사용됨
 public class FoodProperty : MonoBehaviour
 {
-    int VectorSize = 10;
+        int VectorSize = 10;
 
-    //easy
-    private float[] RedProperty = { 1f, 1f, 1f, 1f, 1f, 0f, 0f, 0f, 0f, 0f };
-    private float[] BlueProperty = { 0f, 0f, 0f, 0f, 0f, 1f, 1f, 1f, 1f, 1f };
+        //easy
+        private float[] RedProperty = { 1f, 1f, 1f, 1f, 1f, 0f, 0f, 0f, 0f, 0f };
+        private float[] BlueProperty = { 0f, 0f, 0f, 0f, 0f, 1f, 1f, 1f, 1f, 1f };
 
-    //mid
-    //private float[] BlueProperty = { 9f, 8f, 7f, 6f, 5f, 0f, 0f, 0f, 0f, 0f };
-    //private float[] RedProperty = { 0f, 0f, 0f, 0f, 0f, 9f, 8f, 7f, 6f, 5f };
+        //mid
+        //private float[] BlueProperty = { 9f, 8f, 7f, 6f, 5f, 0f, 0f, 0f, 0f, 0f };
+        //private float[] RedProperty = { 0f, 0f, 0f, 0f, 0f, 9f, 8f, 7f, 6f, 5f };
 
-    //hard
-    //private float[] BlueProperty = { 0f, 1f, 2f, 3f, 4f, 5f, 6f, 7f, 8f, 9f };
-    //private float[] RedProperty = { 9f, 8f, 7f, 6f, 5f, 4f, 3f, 2f, 1f, 0f };
+        //hard
+        //private float[] BlueProperty = { 0f, 1f, 2f, 3f, 4f, 5f, 6f, 7f, 8f, 9f };
+        //private float[] RedProperty = { 9f, 8f, 7f, 6f, 5f, 4f, 3f, 2f, 1f, 0f };
 
-    public float[] FoodP { get; private set; }
+        public float[] FoodP { get; private set; }
 
-    // 음식이 가진 property 초기화 함수 (벡터 성분 초기화)
-    public void InitializeProperties()
-    {
-        if (gameObject.CompareTag("food_red"))
+        // 음식이 가진 property 초기화 함수 (벡터 성분 초기화)
+        public void InitializeProperties()
         {
-            FoodP = AddNoise(RedProperty);
-        }
-        // else if (gameObject.CompareTag("food_blue"))
-        // {
-        //     FoodP = AddNoise(BlueProperty);
-        // }
-       
-    }
+                if (gameObject.CompareTag("food_red"))
+                {
+                        FoodP = AddNoise(RedProperty);
+                }
+                else if (gameObject.CompareTag("food_blue"))
+                {
+                        FoodP = AddNoise(BlueProperty);
+                }
 
-    // Sniffing을 할 때 음식에 대한 noise 설정 함수
-    private float[] AddNoise(float[] property)
-    {
-        for (int i = 0; i < VectorSize; i++)
+        }
+
+        // Sniffing을 할 때 음식에 대한 noise 설정 함수
+        private float[] AddNoise(float[] property)
         {
-            //easy
-            property[i] += 0f;
+                for (int i = 0; i < VectorSize; i++)
+                {
+                        //easy
+                        property[i] += 0f;
 
-            // Gaussian
-            // float noise = (float)Generate.Normal(1, 0, 1)[0];
+                        // Gaussian
+                        // float noise = (float)Generate.Normal(1, 0, 1)[0];
 
-            // Uniform(0~1)
-            //System.Random r = new System.Random();
-            //double noise = r.NextDouble();
-            //property[i] += (float)noise;
+                        // Uniform(0~1)
+                        //System.Random r = new System.Random();
+                        //double noise = r.NextDouble();
+                        //property[i] += (float)noise;
+                }
+                return property;
         }
-        return property;
-    }
 }
