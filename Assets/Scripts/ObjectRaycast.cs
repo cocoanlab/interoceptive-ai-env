9456e5408199fa9d760cc9ac006d4404a48de7ef
SoHee Kim <thgml042762@gmail.com>
2023-08-11 11:30:37 +0900

    Sohee added Object Raycast

--- Assets/Scripts/ObjectRaycast.cs
+++ Assets/Scripts/ObjectRaycast.cs
@@ -1,30 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Unity.MLAgents.Sensors;
 
 public class ObjectRaycast : MonoBehaviour
 {
-
     public GameObject agent;
-    [SerializeField]
-    
-    public RaycastHit hit;
-    public float Maxdistance = 10;
     public LayerMask layermask;
+    public int raysPerDirection = 20;
+    public float maxDistance = 5;
+    public float radialRange = 100f;
+    public RaycastHit hit;
 
     void Update()
     {
-        Vector3 direction = transform.forward;
-
-        Debug.DrawRay(transform.position, direction * Maxdistance, Color.green);
-
-        if(Physics.Raycast(transform.position, direction, out hit, Maxdistance, layermask))
+        for (int i = 0; i < raysPerDirection; i++)
         {
-            print(hit.transform.name);
-            agent.GetComponent<InteroceptiveAgent>().Collision();
+            float angle = i * radialRange / (raysPerDirection - 1); // Distribute angles evenly within the range
+            Quaternion rotation = Quaternion.Euler(0f, angle - radialRange / 2, 0f); // Adjust angle offset
+            Vector3 direction = rotation * transform.forward;
 
+        
+            if (Physics.Raycast(transform.position, direction, out hit, maxDistance, layermask))
+            {
+                Debug.DrawRay(transform.position, direction * maxDistance, Color.red);
+                Debug.Log(hit.transform.name);
+                agent.GetComponent<InteroceptiveAgent>().Collision();
+            }
+            else
+            {
+                Debug.DrawRay(transform.position, direction * maxDistance, Color.green);
+            }
         }
     }
 }
-    
-
